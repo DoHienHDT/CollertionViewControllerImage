44b776fe6094b34ae68d5a77b749c8c7d3911dd7
dohien <dohien11091999@gmail.com>
2018-08-01 10:36:04 +0700

    add image

---
+++ CollertionViewControllerImage/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollertionViewControllerImage
+//
+//  Created by dohien on 01/08/2018.
+//  Copyright © 2018 dohien. All rights reserved.
+//
+
+import UIKit
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImage: UIImageView!
+    
+}
---
+++ CollertionViewControllerImage/CollectionViewController.swift
@@ -0,0 +1,65 @@
+//
+//  CollectionViewController.swift
+//  CollertionViewControllerImage
+//
+//  Created by dohien on 01/08/2018.
+//  Copyright © 2018 dohien. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+    let padding: CGFloat = 8
+    let numberOfItem: CGFloat = 5
+    var numbers = Array(1...12)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return numbers.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+        cell.photoImage.image = #imageLiteral(resourceName: "Apple")
+        return cell
+    }
+
+}
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        
+        let cellSquareSize = (screenWidth - 2 * padding - ((numberOfItem - 1) * padding)) / numberOfItem
+        
+        return CGSize.init(width: cellSquareSize, height: cellSquareSize)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return padding
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return padding
+    }
+
+}
